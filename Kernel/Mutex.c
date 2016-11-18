16b5a7c709ed73a3e9e4ee2c8c22bc9379294470
msaques <msaques@itba.edu.ar>
2016-11-18 12:45:16 -0300

    working on mutex

--- Kernel/Mutex.c
+++ Kernel/Mutex.c
@@ -1,10 +1,22 @@
 #include <Mutex.h>
 #include <asmlib.h>
+#include <trylock.h>
+#include <Scheduler.h>
+
 #define NULL 0
 
+struct pidnode {
+	uint64_t pid;
+	struct pidnode * next;
+};
+
+typedef struct pidnode pidnode;
 
 struct MutexNode {
 	mutex * m;
+	pidnode * first;
+	pidnode * last;
+	uint64_t waitn;
 	struct MutexNode * next;
 };
 
@@ -12,6 +24,7 @@ typedef struct MutexNode MutexNode;
 
 extern int64_t strcmp(const uint8_t * a, const uint8_t * b) ;
 static MutexNode * mutex_destroy_rec(MutexNode * n,mutex * m);
+static MutexNode * mutexnode_getbyid(uint64_t id);
 static mutex * minit(mutex * m,uint8_t * name);
 static uint64_t mutex_count = 0;
 static uint64_t historic = 0;
@@ -26,15 +39,23 @@ uint64_t mutex_nameinit(mutex * m,uint8_t * name){
 }
 
 static mutex * minit(mutex * m,uint8_t * name){
+	disableScheduler();
+	
 	m->m = 0;
 	m->id = historic;
 	m->name = name;
 	MutexNode * n = la_malloc(sizeof(MutexNode));
 	n->m = m;
 	n->next = nodes;
+	
+	n->waitn = 0;
+	n->first = n->last = NULL;
+	
 	nodes = n;
 	mutex_count ++;
 	historic ++;
+	
+	enableScheduler();
 	return m;
 }
 
@@ -48,8 +69,20 @@ mutex * mutex_getbyname(uint8_t * name){
 	return NULL;
 }
 
+static MutexNode * mutexnode_getbyid(uint64_t id){
+	MutexNode * n = nodes;
+	for(; n != NULL ; n = n->next){
+		if(n->m->id == id){
+			return n;
+		}
+	}
+	return NULL;
+}
+
 void mutex_destroy(mutex * m){
+	disableScheduler();
 	nodes = mutex_destroy_rec(nodes,m);
+	enableScheduler();
 }
 
 static MutexNode * mutex_destroy_rec(MutexNode * n,mutex * m){
@@ -67,9 +100,41 @@ static MutexNode * mutex_destroy_rec(MutexNode * n,mutex * m){
 }
 
 void mutex_lock(mutex * m){
-	enter_region(&(m->m));
+	if(!itrylock(&(m->m))){
+		disableScheduler();
+		MutexNode * mn = mutexnode_getbyid(m->id);
+		pidnode * pidn = la_malloc(sizeof(pidnode));
+		pidn->pid = currPid();
+		if(mn->waitn == 0){
+			pidn->next = NULL;
+			mn->first = mn->last = pidn;
+		} else {
+			mn->last->next = pidn;
+			pidn->next = NULL;
+			mn->last = pidn;
+		}
+		mn->waitn ++;
+		enableScheduler();
+		wait();
+	}
 }
 
 void mutex_unlock(mutex * m){
-	leave_region(&(m->m));
+	disableScheduler();
+	MutexNode * mn = mutexnode_getbyid(m->id);
+	if(mn->waitn > 0){
+		pidnode * pidn = mn->first;
+		wake(pidn->pid);
+		if(mn->waitn == 1){
+			mn->first = mn->last = NULL;
+		} else {
+			mn->first = pidn->next;
+		}
+		la_free(pidn);
+		mn->waitn --;
+	} else {
+		itryunlock(&(m->m));
+	}
+	enableScheduler();
+	
 }
